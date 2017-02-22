729466d9d06017ae8c8dcc4f941ddd39198751d1
Eric Harris-Braun <eric@harris-braun.com>
2017-02-22 08:37:38 -0500

    more imports cleanup, another dup.

--- echo/main.go
+++ echo/main.go
@@ -10,7 +10,6 @@ import (
 
 	golog "github.com/ipfs/go-log"
 	host "github.com/libp2p/go-libp2p-host"
-	inet "github.com/libp2p/go-libp2p-net"
 	net "github.com/libp2p/go-libp2p-net"
 	peer "github.com/libp2p/go-libp2p-peer"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
@@ -142,7 +141,7 @@ func main() {
 
 // doEcho reads some data from a stream, writes it back and closes the
 // stream.
-func doEcho(s inet.Stream) {
+func doEcho(s net.Stream) {
 	buf := make([]byte, 1024)
 	n, err := s.Read(buf)
 	if err != nil {
