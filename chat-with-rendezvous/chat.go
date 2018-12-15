84dd120e4f580f77f33f650b419beb8c8104263c
Matt Joiner <anacrolix@gmail.com>
2018-12-15 13:40:46 +1100

    Fix usage printing
    License: MIT
    Signed-off-by: Matt Joiner <anacrolix@gmail.com>

--- chat-with-rendezvous/chat.go
+++ chat-with-rendezvous/chat.go
@@ -83,10 +83,11 @@ func main() {
 	}
 
 	if *help {
-		fmt.Printf("This program demonstrates a simple p2p chat application using libp2p\n\n")
-		fmt.Printf("Usage: Run './chat in two different terminals. Let them connect to the bootstrap nodes, announce themselves and connect to the peers\n")
-
-		os.Exit(0)
+		fmt.Println("This program demonstrates a simple p2p chat application using libp2p")
+		fmt.Println()
+		fmt.Println("Usage: Run './chat in two different terminals. Let them connect to the bootstrap nodes, announce themselves and connect to the peers")
+		flag.PrintDefaults()
+		return
 	}
 
 	ctx := context.Background()
